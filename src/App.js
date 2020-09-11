aba9bd7a9d85b70fa791b7619bf4279bc991a355
Higor <higorm22@hotmail.com>
2020-09-11 11:04:30 -0300

    Desafio repositories React Frontend - Finalizado

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState }from "react";
+
+import "./styles.css";
+import api from "./services/api";
+
+function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then( response => {
+      setRepositories(response.data);
+    });    
+  }, [])
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: 'Higorm22',
+      url: 'https://github.com/rocketseat/higorm22',
+      techs: ['Node.js','ReactJS']
+    })
+
+    setRepositories([...repositories, response.data]);
+  }
+
+  async function handleRemoveRepository(id) {
+    // TODO
+    await api.delete(`repositories/${id}`,);
+
+    setRepositories(repositories.filter(
+      repository => repository.id ==! id
+    ))
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button> 
+        </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
